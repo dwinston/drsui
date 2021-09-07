ace5c952a21e0ba3f2cb14b7997174d3238589fe
Donny Winston <donny@polyneme.xyz>
2021-09-07 12:18:07 -0400

    progress!

--- src/Main.elm
+++ src/Main.elm
@@ -1,31 +1,75 @@
 module Main exposing (..)
 
-import Browser
-import Html
-import Browser.Navigation exposing (Key)
+import Browser exposing (UrlRequest)
+import Browser.Navigation as Navigation exposing (Key)
+import Html exposing (Html)
+import Html.Attributes as Attrs
+import Route exposing (Route)
+import Url exposing (Url)
 
 
-init flags url key = ( {key = key}, Cmd.none)
+init : () -> Url -> Key -> ( Model, Cmd Msg )
+init _ url key =
+    ( { key = key, route = Route.parse url }, Cmd.none )
 
-type Msg = TODO
+
+type Msg
+    = UrlRequest Browser.UrlRequest
+    | UrlChange Url
+
 
 type alias Model =
-    {key : Key
+    { key : Key
+    , route : Route
     }
-update : Msg -> Model -> (Model, Cmd Msg)
-update msg model = ( model, Cmd.none)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        UrlRequest (Browser.Internal url) ->
+            ( model, Navigation.pushUrl model.key (Url.toString url) )
+
+        UrlRequest (Browser.External str) ->
+            ( model, Navigation.load str )
+
+        UrlChange url ->
+            ( { model | route = Route.parse url }, Cmd.none )
+
 
 view : Model -> Browser.Document Msg
-view model = {title = "hello", body = [] }
-
-
-
-main: Program () Model Msg
-main = Browser.application
-    {init = init
-    ,view = view
-    ,update = update
-    ,subscriptions = \_ -> Sub.none
-    ,onUrlRequest = \_ -> Debug.todo "onUrlRequest"
-    ,onUrlChange = \_ -> Debug.todo "onUrlRequest"
+view model =
+    { title = "hello"
+    , body =
+        [ Html.header []
+            [ Html.div [] [ Html.text "Header" ]
+            , Html.nav []
+                [ Html.a [ Attrs.href "/login" ] [ Html.text "Sign In" ] ]
+            ]
+        , Html.main_ [] [ viewPageContent model ]
+        , Html.text <| Debug.toString model
+        , Html.footer [] [ Html.text "Footer" ]
+        ]
     }
+
+
+viewPageContent : Model -> Html Msg
+viewPageContent model =
+    case model.route of
+        Route.Login ->
+            Html.text "Login"
+
+        Route.NotFound ->
+            Html.text "NotFound"
+
+
+main : Program () Model Msg
+main =
+    Browser.application
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        , onUrlRequest = UrlRequest
+        , onUrlChange = UrlChange
+        }
--- src/Route.elm
+++ src/Route.elm
@@ -1,16 +1,25 @@
-module Route exposing (..)
-import Url.Parser as Parser exposing ((</>), Parser)
+module Route exposing (Route(..), parse)
+
 import Url exposing (Url)
+import Url.Parser as Parser exposing ((</>), Parser)
+
+
 type Route
     = Login
     | NotFound
 
+
 parse : Url -> Route
-parse url = Parser.parse parser url |> Maybe.withDefault NotFound
+parse url =
+    Parser.parse parser url |> Maybe.withDefault NotFound
+
 
 parser : Parser (Route -> a) a
-parser = Parser.oneOf [
-    Parser.map Login (Parser.top </> Parser.s "login")
-    ]
+parser =
+    Parser.oneOf
+        [ Parser.map Login (Parser.top </> Parser.s "login")
+        ]
+
+
 
 -- TODO Url.toString or Url.fromString
